19c321744bd462942e21b1c261e604f6dea208f9
Wout Neirynck <wout.neirynck@d-n.be>
2020-02-12 08:44:14 +0100

    Release 0.6.1

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject re-frame-datatable "0.6.1-SNAPSHOT"
+(defproject re-frame-datatable "0.6.1"
   :description "DataTable component for re-frame 0.8.0+"
-  :url "https://github.com/kishanov/re-frame-datatable"
+  :url "https://github.com/debreuck-neirynck/re-frame-datatable"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.908"]
-                 [reagent "0.7.0"]
-                 [re-frame "0.10.1"]])
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/clojurescript "1.10.520"]
+                 [reagent "0.8.1"]
+                 [re-frame "0.10.6"]])
